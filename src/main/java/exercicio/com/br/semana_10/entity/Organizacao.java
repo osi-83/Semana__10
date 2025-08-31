e976ef9ae8edccb863ae6f5d231643eb61e87242
Osiel Fernandes <osiel.fernandes.1983@gmail.com>
2025-08-31 03:15:51 -0300

    create class users e org

--- src/main/java/exercicio/com/br/semana_10/entity/Organizacao.java
+++ src/main/java/exercicio/com/br/semana_10/entity/Organizacao.java
@@ -1,4 +1,25 @@
 package exercicio.com.br.semana_10.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "organizacoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Organizacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 100)
+    private String contato;
+
 }
--- src/main/java/exercicio/com/br/semana_10/entity/Usuario.java
+++ src/main/java/exercicio/com/br/semana_10/entity/Usuario.java
@@ -1,4 +1,35 @@
 package exercicio.com.br.semana_10.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Perfil perfil;
+
+    public enum Perfil {
+        ADMIN, USER
+    }
 }
